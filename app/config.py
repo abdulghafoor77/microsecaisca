49d4204bfb6c96e22eb59b462e6ea75b2053643c
abdul.ghafoor <abdul.ghafoor@ri.se>
2026-04-27 15:23:28 +0200

    Fixed

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 DB_CONFIG = {
     'user': 'dbuser',
-    'password': 'Azby1029', # nosec B105
+    'password': 'asasas', # nosec
     'host': 'localhost',
     'database': 'acme_db',  # Your desired DB name
 }
